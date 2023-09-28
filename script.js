413d19d182df5604953388ca23568c178ce07abd
151278deepanshu <deepanshugupta151278@gmail.com>
2023-09-28 13:41:09 +0530

    change resume

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 const downloadDiv = document.getElementById("downloadDiv");
 
 // Define the file URL of your resume
-const resumeUrl = "resume.pdf"; // Change to your actual resume file URL
+const resumeUrl = "myresume.pdf"; // Change to your actual resume file URL
 
 // Add a click event listener to the div
 downloadDiv.addEventListener("click", () => {
     // Create an anchor element to trigger the download
     const anchor = document.createElement("a");
     anchor.href = resumeUrl;
-    anchor.download = "resume.pdf"; // Change to your desired download filename
+    anchor.download = "myresume.pdf"; // Change to your desired download filename
 
     // Trigger a click event on the anchor element
     anchor.click();
